a6a84f5ddbbc8cf4dee1f5369d7f613cde2b6889
Afomike <temidayoafote@gmail.com>
2026-07-16 20:28:08 +0100

    correct error

--- dist/public/assets/imagebg_3-zfFR6Syh.js
+++
@@ -1,1 +0,0 @@
-const a="/assets/imagebg_3-CF3kh_3w.png";export{a};
--- dist/public/assets/skeleton-BmwbqsUC.js
+++
@@ -1,1 +0,0 @@
-import{j as s,q as a}from"./index-D1SVuU9A.js";function t({className:e,...r}){return s.jsx("div",{className:a("animate-pulse rounded-md bg-primary/10",e),...r})}export{t as S};
